63327276ff5421055cae01182da2f9b573b4249c
Andmyr <adm.dominant.plaza@gmail.com>
2023-10-19 18:34:39 +0300

    add file for functional

---
+++ contact.py
@@ -0,0 +1,224 @@
+from collections import UserDict
+from datetime import datetime, timedelta
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __str__(self):
+        return f"Name: {self.value}"
+
+
+class Address(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return f"Address: {self.value}"
+
+
+class Email(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value = self.is_data(value)
+
+    def is_data(self, email):
+        try:
+            email = re.findall(r"[a-zA-Z][a-zA-Z._0-9]+@\w+\.+\w\w+", email)
+            if email == []:
+                raise ValueError ("Введено неправильний e-mail")
+        except:
+            return None
+        return email
+
+    def __str__(self):
+        return f"Email: {self.value}"
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = self.is_data(value)
+
+    def is_data(self, value):
+        if value == "":
+            return value
+        value = value.replace(".", "/")
+        day_pattern = r"\d{2}/\d{2}/\d{4}"
+        try:
+            if not re.match(day_pattern, value):
+                raise ValueError("неправильний формат дати")
+            datetime.strptime(value, "%d/%m/%Y")
+            return value
+        except ValueError:
+            print("неправильно вказано дату")
+            return ""
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = self.is_data(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        try:
+            self.value = self.is_phone(value)
+        except ValueError:
+            print("неправильний номер телефону _")
+            self.value = ""
+    
+    def __str__(self):
+        return f"Phone: {self.value}"
+    
+    def is_phone(self, value):
+        phone_pattern_1 = r"^[0-9]{10}$"
+        phone_pattern_2 = r"^[0-9]{12}$"
+        if (re.match(phone_pattern_1, value) == re.match(phone_pattern_2, value)):
+            return ""
+        return value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = self.is_phone(value)
+
+
+class Record:
+    def __init__(self, name, birthday = ""):   
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday(birthday)
+        self.address = None  
+        self.email = None 
+
+    def __str__(self):
+        if self.birthday.value:
+            return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, Birthday: {self.birthday.value}, address: {self.address.value}, e-mail: {self.email.value}"
+        else:
+            return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def add_phone(self, phone):
+        for i in self.phones:
+            if str(i) == phone:
+                return False
+        result = Phone(phone)
+        if result.value:
+            self.phones.append(result)
+
+    def add_address(self, address):
+        self.address = Address(address)
+
+    def add_email(self, email):
+        self.email = Email(email)
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if str(p.value) != phone]
+
+    def edit_phone(self, phone_old, phone_new):
+        if self.find_phone(phone_old):
+            self.remove_phone(phone_old)
+            self.add_phone(phone_new)
+
+    def find_phone(self, phone):
+        for i in self.phones:
+            if str(i) == phone:
+                return Phone(phone)
+        return None
+
+    def days_to_birthday(self):
+        if not self.birthday:
+            return 0
+        current_data = datetime.now().date()
+        birthday_date = datetime.strptime(str(self.birthday.value), "%d/%m/%Y").date()
+        birthday_date_new = birthday_date.replace(year=current_data.year)
+        if birthday_date_new >= current_data:
+            return (birthday_date_new - current_data).days
+        else:
+            return (birthday_date.replace(year=(current_data.year + 1)) - current_data).days
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, value):
+        self.data[value.name.value] = value
+
+    def find(self, value):
+        return self.data.get(value)
+    
+    def find_contact(self, value):
+        result = [self.data.get(contact) for contact in self.data if value in contact]
+        return result
+    
+    def find_all(self, value):
+        list_contact = [str(self.data.get(contact)) for contact in self.data]
+        result = [contact for contact in list_contact if value in str(contact)]
+        return result
+
+    def delete(self, value):
+        for key, i in self.data.items():
+            if value in str(i):
+                return self.data.pop(key, "і не було")
+
+    def show_book(self, count=0, list=False):
+        # вивід адресної книги
+        # count = 0 - вся книга, або вказана кількість строк
+        # list = False - тільки вказана кількості строк, True - вся книга, але з розбиттям по вказаній кількості строк
+        count = count if count >= 0 else 0
+        stop_check = count
+        msg = None
+        list = list if list == False else True
+        records_iterator = iter(self.data.values())
+        for i in range(len(self.data)):
+            contact = next(records_iterator, None)
+            if contact is None:
+                break
+            print(contact)
+            stop_check -= 1
+            if list:
+                if not stop_check:
+                    msg = input("Для продовження 'Enter', Вихід - любий символ ")
+                    stop_check = count
+                if msg:
+                    return
+            else:
+                if not stop_check:
+                    return
+
+    def contact_for_birthday(self, days):
+        now=datetime.now().date()
+        result=''
+        
+        days_interval=timedelta(days=days)
+        for key, value in self.data.items():
+            if value.birthday.value:
+                date = datetime(year=now.year, month=now.month, day=now.day)+days_interval
+                birthday_date = datetime.strptime(str(value.birthday.value), "%d/%m/%Y").date()
+                birthday_date_new = birthday_date.replace(year=date.year)
+                if date.date()==birthday_date_new:
+                    result+=f'Contact name: {key} phones:'+str(*value.phones)+'\n'
+        return result
+
+    def __str__(self):
+        print(f"\nAddressBook list name: ")
+        for i in self.data.values():
+            print(i)
+        return "AddressBook"
+
--- func.py
+++ func.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timedelta
 import pickle
 import re
+from contact import Field, Name, Address, Email, Birthday, Phone, Record, AddressBook
+from notepad import Note, Note_book
+import clean_folder
 
+
+global address_book
+global note_book
 
 def input_error(func):
     def inner(*args, **kwargs):
@@ -30,3 +36,214 @@ def exit_boot(address_book, note_book):
         pickle.dump(note_book, fh)
 
     return "exit"
+
+def hello(*arg):
+    print("How can I help you?")
+
+
+def add_contact(address_book,*arg):
+    name = input_data("Введіть ім'я контакту: ")
+    if name == "":
+        return
+    user = Record(name)
+    added = False
+    while True:
+        phone = input_data("Введіть номер телефону (xxxxxxxxxx): ")
+        if phone == "" :
+            break
+        phone_value = Phone(phone)
+        if phone_value.value =="": 
+            print ("Не вірно вказано номер телефлефону, повторіть ввід або вийти Enter")
+        else:
+            #user.add_phone(phone_value.value)
+            user.add_phone(phone)
+            added = True
+            break
+    while True:
+        birthday = input_data("Введіть дату дня народження (dd/mm/yyyy): ")
+        if birthday == "" :
+            break
+        birthday_value = Birthday(birthday)
+        if birthday_value.value == "": 
+            print ("Не вірно вказано дату дня народження, повторіть ввід або вийти Enter")
+        else:
+            user.add_birthday(birthday)
+            added = True
+            break
+    while True:
+        email = input_data("Введіть e-mail: ")
+        if email == "" :
+            break
+        email_value = Email(email)
+        if email_value.value == None: 
+            print ("Не вірно вказано e-mail, повторіть ввід або вийти Enter")
+        else:
+            user.add_email(email)
+            added = True
+            break
+    if added:
+        address = input_data("Введіть адресу: ")
+        user.add_address(address)
+        address_book.add_record(user)
+        print(f"Додано контакт : {user}")
+
+
+def change_phone(address_book,*arg):
+    name = input_data("Введіть ім'я (Вийти 'Enter') \n")
+    contact = address_book.find(name)
+    if name == "":
+        return None
+    elif contact is None:
+        print("Немає такого контакту")
+        return None
+    print(contact)
+    phone = input_data("Введіть номер телефону який необхідно замінити (xxxxxxxxxx) (Вийти 'no') \n")
+    if phone == 'no':
+        return None
+    elif Phone(phone).value:
+        phone_new = input_data("Введіть новий номер телефону (xxxxxxxxxx) (Вийти 'no') \n")
+        if phone_new == 'no':
+            return None
+        elif Phone(phone_new).value:
+            contact = address_book.get(name)
+            contact.remove_phone(phone)  # Видалити старий номер телефону
+            contact.add_phone(phone_new)  # Додати новий номер телефону
+            address_book.data[name] = contact  # Оновити контакт у address_book
+            print(f"Змінено контакт: {contact}, новий номер телефону: {phone_new}")
+
+
+def change_email(address_book,*arg):
+    name = input_data("Введіть ім'я (Вийти 'Enter') \n")
+    contact = address_book.find(name)
+    if name == "":
+        return None
+    elif contact is None:
+        print("Немає такого контакту")
+        return None
+    print(contact)
+    email = input_data("Введіть e-mail який необхідно замінити (Вийти 'no') \n")
+    if email == 'no':
+        return None
+    elif Email(email).value:
+        email_new = input_data("Введіть новий номер телефону (xxxxxxxxxx) (Вийти 'no') \n")
+        if email_new == 'no':
+            return None
+        elif Email(email_new).value:
+            contact = address_book.get(name)
+            #contact.remove_phone(phone)  # Видалити старий номер телефону
+            contact.add_email(email_new)  # Додати новий номер телефону
+            address_book.data[name] = contact  # Оновити контакт у address_book
+            print(f"Змінено контакт: {contact}, новий e-mail: {email_new}")
+ 
+
+def find_contact(address_book,*arg):
+    result = input_data("Введіть назву контакту або його частину: ")
+    list_contact = address_book.find_contact(result)
+    if list_contact == []:
+        print ("Такого контакту немає")
+        return None
+    for contact in list_contact:
+        print(contact)
+
+
+def find_all(address_book,*arg):
+    result = input_data("Введіть значення пошуку: ")
+    list_contact = address_book.find_all(result)
+    if list_contact == []:
+        print ("За даними умовами пошуку нічого не знайдено")
+        return None
+    for contact in list_contact:
+        print(contact)
+
+
+def show_all(address_book,*arg):
+    print("Вивід всіх збеорежених контактів \n")
+    #address_book.show_book(0, list=False)
+    for value in address_book.values():
+        print(value)
+
+
+def when_birthday(address_book,*arg):
+    result = input_data("Вкажіть ім'я контакту/(Вийти 'Enter')\n")
+    if result=='':
+        return
+    else:
+        if result in address_book and address_book[result].days_to_birthday():
+            if address_book[result].days_to_birthday():
+                print(address_book[result].days_to_birthday())
+        else:
+            print(f"не має запису в адресній книзі для: {result}")
+
+@input_error
+def contacts_birthday(address_book,*arg):
+    result = input_data("Вкажіть кількість днів/(Вийти 'Enter')\n")
+    if result=='':
+        return
+    try:
+        days=int(result)
+    except:
+        raise ValueError('Введіть дні числом')
+    if address_book.contact_for_birthday(int(result)):
+        print(address_book.contact_for_birthday(int(result)))
+    else:
+        print(f'Через {days} днів ні в кого немає день народження')
+
+
+def cleans_folder(*arg):
+    result = input_data("Вкажіть шлях до папки/(Вийти 'Enter')\n")
+    if result=='':
+        return
+    else:
+        try:
+            clean_folder.main(result)
+        except:
+            print(f'Невірний шлях до папки')
+
+
+def good_bye(address_book, note_book,*arg):
+    exit_boot(address_book.data, note_book.data)
+    return "Good bye!"
+
+def add_note(note_book,*arg):
+    while True:
+        result=input_data("Напишіть ключове слово(#..)/їх може бути декілька/\n")
+        if result=='' or result[0]!='#':
+            print('Некоректно введено тег')
+        else:
+            break
+    text=[]
+    print('Напишіть саму нотатку')
+    while True:
+        result2=input()
+        if result2=='':
+            break
+        text.append(result2)
+    if text:
+        text = '\n'.join(text)
+        note=Note(text,result)
+        note_book.add_note(note)
+
+
+def find_note(note_book,*arg):
+    result = input_data("Напишіть ключове слово(#..)/їх може бути декілька/Вийти no\n")
+    if result=='':
+        return
+    matches = note_book.find_notes(result)
+    if matches:
+        for i in matches:
+            print(i)
+    else:
+        print("Немає нотатків по такому тегу")
+        command='find_note'
+
+
+def sort_notes(note_book,*arg):
+    note_book.sort_notes()
+
+
+def show_notes(note_book,*arg):
+    if note_book.data:
+        for tags in note_book.data:
+            print('#'+tags)
+            for note in note_book.data[tags]:
+                print(f"{note.text}")
